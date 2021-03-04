f34175fb9325bd681111d8f0e9c976813c1c62f2
Jaehwan <chrisjae508@naver.com>
2021-03-04 20:54:51 +0900

    first commit

---
+++ pybo/views/main_views.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+
+@bp.route('/')
+def index():
+    return redirect(url_for('question._list'))  # '블루프린트 이름.블루프린트에 등록된 함수명'
