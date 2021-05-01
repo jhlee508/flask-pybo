a3832b1adb86c45adacd847426a52a65822a579b
Jaehwan <chrisjae508@naver.com>
2021-05-01 20:15:17 +0900

    [Refactor] 수정 및 삭제 기능

--- pybo/models.py
+++ pybo/models.py
@@ -9,6 +9,7 @@ class Question(db.Model):
     create_date = db.Column(db.DateTime(), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('question_set'))
+    modify_date = db.Column(db.DateTime(), nullable=True)
 
 
 # 답변 모델
@@ -20,6 +21,7 @@ class Answer(db.Model):
     create_date = db.Column(db.DateTime(), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('answer_set'))
+    modify_date = db.Column(db.DateTime(), nullable=True)
 
 
 # 회원 정보 모델
